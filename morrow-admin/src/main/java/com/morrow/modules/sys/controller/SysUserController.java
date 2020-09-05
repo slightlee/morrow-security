08b8192d9bf73175cd5bacded170edcb9e140b6f
morrow <lmm_work@163.com>
2020-09-05 17:33:36 +0800

    模块拆分 @V1.0.1

--- morrow-admin/src/main/java/com/morrow/modules/sys/controller/SysUserController.java
+++ morrow-admin/src/main/java/com/morrow/modules/sys/controller/SysUserController.java
@@ -1,9 +1,15 @@
 package com.morrow.modules.sys.controller;
 
 
+import com.morrow.modules.sys.entity.SysUser;
+import com.morrow.modules.sys.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sys/user")
 public class SysUserController {
 
+
+    @Autowired
+    SysUserService sysUserService;
+
+
+    @GetMapping("/list")
+    public Object list(){
+        List<SysUser> list = sysUserService.list();
+        return list;
+    }
 }
